233dc84b93cd3ea2619c3933409129a1fdf9fdda
Graig Janssen <graig.janssen@gmail.com>
2018-05-17 19:23:27 -0400

    Game mechanics working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Faces } from './faces';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,91 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  gameStart: boolean;
+  faces: {}[];
+  faceIndex: number;
+  total: number;
+  available: number;
+  numFaces: number;
+  answer: string = '';
+  score: number;
+  message: string;
+  success: boolean;
+  finalScore: number;
+  finalText: string = '';
+
+  constructor() {
+    this.gameStart = false;
+    this.available = Faces.length;
+    this.faces = Faces.slice(0);
+    this.faceIndex = 0;
+    this.total = this.faces.length;
+  }
+
+  onStartClick() {
+    this.faces = this.prepareFaces();
+    this.total = this.faces.length;
+    this.faceIndex = 0;
+    this.score = 0;
+    this.message = '';
+    this.gameStart = true;
+  }
+
+  onSubmit() {
+    if (this.answer.length > 0) {
+      const guess = this.answer;
+      const name = this.faces[this.faceIndex]['name'];
+      if (guess.toLowerCase() === name.toLowerCase()) {
+        this.score++;
+        this.success = true;
+        this.message = name + ' is right!';
+      } else {
+        this.success = false;
+        this.message = `Oops, it's not ${guess}, it's ${name}.`;
+      }
+      this.answer = '';
+      this.faceIndex++;
+
+      if (this.faceIndex === this.total) {
+        this.finalScore = (this.score / this.total) * 100;
+        this.finalText = `You got ${this.score} out of ${this.total}`;
+      }
+    } else {
+      this.success = false;
+      this.message = "Make a guess!";
+    }
+  }
+
+  newGame() {
+    this.gameStart = false;
+    this.faces = Faces.slice(0);
+    this.numFaces = null;
+    this.message = '';
+  }
+
+  onMax() {
+    this.numFaces = Faces.length;
+    this.onStartClick();
+  }
+
+  prepareFaces() {
+    const faces = Faces.slice(0);
+    if (typeof this.numFaces === 'number' && this.numFaces > 0 && this.numFaces <= faces.length) {
+      const shuffledFaces = this.shuffleFaces(faces);
+      while (shuffledFaces.length > this.numFaces) {
+        shuffledFaces.pop();
+      }
+      return shuffledFaces;
+    } else {
+      return this.shuffleFaces(faces);
+    }
+  }
+
+  shuffleFaces(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
 }
---
+++ src/app/faces.ts
@@ -0,0 +1,42 @@
+export const Faces = [
+  {
+    name: 'Jennifer Aniston',
+    src: 'assets/img/jennifer-aniston.jpeg'
+  },
+  {
+    name: 'Alec Baldwin',
+    src: 'assets/img/alec-baldwin.jpeg'
+  },
+  {
+    name: 'Bradley Cooper',
+    src: 'assets/img/bradley-cooper.jpeg'
+  },
+  {
+    name: 'Robert De Niro',
+    src: 'assets/img/robert-de-niro.jpeg'
+  },
+  {
+    name: 'Clint Eastwood',
+    src: 'assets/img/clint-eastwood.jpg'
+  },
+  {
+    name: 'Jodie Foster',
+    src: 'assets/img/jodie-foster.jpeg'
+  },
+  {
+    name: 'Jennifer Garner',
+    src: 'assets/img/jennifer-garner.jpeg'
+  },
+  {
+    name: 'Tom Hanks',
+    src: 'assets/img/tom-hanks.jpeg'
+  },
+  {
+    name: 'Sylvester Stallone',
+    src: 'assets/img/sylvester-stallone.jpg'
+  },
+  {
+    name: 'Meryl Streep',
+    src: 'assets/img/meryl-streep.jpeg'
+  }
+];
